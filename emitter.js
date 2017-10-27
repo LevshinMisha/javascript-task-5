5549b7baea2ccae6b5bde77924f4b43b92903c7a
LevshinMisha <LevshinMisha@gmail.com>
2017-10-28 01:05:21 +0500

    готово

--- emitter.js
+++ emitter.js
@@ -1,68 +1,82 @@
 'use strict';
 
-/**
- * Сделано задание на звездочку
- * Реализованы методы several и through
- */
 getEmitter.isStar = true;
 module.exports = getEmitter;
 
-/**
- * Возвращает новый emitter
- * @returns {Object}
- */
+class Listener {
+    constructor(event, context, handler, { several, through }) {
+        this.event = event;
+        this.context = context;
+        this.handler = handler;
+        this.several = several;
+        this.through = through;
+        this.on = true;
+        this.count = 0;
+    }
+
+    notify() {
+        if (this.on && this.several - this.count !== 0) {
+            if (this.count % this.through === 0) {
+                this.handler.call(this.context);
+            }
+            this.count++;
+        }
+    }
+}
+
 function getEmitter() {
     return {
+        listeners: [],
 
-        /**
-         * Подписаться на событие
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         */
+        listen: function (event, context, handler, options) {
+            this.listeners.push(new Listener(event, context, handler, options));
+
+            return this;
+        },
+
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            return this.listen(event, context, handler, { several: -1, through: 1 });
         },
 
-        /**
-         * Отписаться от события
-         * @param {String} event
-         * @param {Object} context
-         */
         off: function (event, context) {
-            console.info(event, context);
+            this.listeners.forEach(listener => {
+                if (listener.context === context && (listener.event.startsWith(event + '.') ||
+                    listener.event === event)) {
+                    listener.on = false;
+                }
+            });
+
+            return this;
         },
 
-        /**
-         * Уведомить о событии
-         * @param {String} event
-         */
         emit: function (event) {
-            console.info(event);
+            this.listeners.forEach(item => {
+                if (item.event === event) {
+                    item.notify();
+                }
+            });
+
+            if (event.includes('.')) {
+                this.emit(event.slice(0, event.lastIndexOf('.')));
+            }
+
+            return this;
         },
 
-        /**
-         * Подписаться на событие с ограничением по количеству полученных уведомлений
-         * @star
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         * @param {Number} times – сколько раз получить уведомление
-         */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            if (times > 0) {
+                return this.listen(event, context, handler, { several: times, through: 1 });
+            }
+
+            return this.on(event, context, handler);
         },
 
-        /**
-         * Подписаться на событие с ограничением по частоте получения уведомлений
-         * @star
-         * @param {String} event
-         * @param {Object} context
-         * @param {Function} handler
-         * @param {Number} frequency – как часто уведомлять
-         */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            if (frequency > 0) {
+                return this.listen(event, context, handler, { several: -1, through: frequency });
+            }
+
+            return this.on(event, context, handler);
         }
     };
 }
