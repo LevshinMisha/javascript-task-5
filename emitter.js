938d8837d826dfc69b981c09eca9456b8460996e
LevshinMisha <LevshinMisha@gmail.com>
2017-10-31 00:15:51 +0500

    Добавил фильтры и звездочку

--- emitter.js
+++ emitter.js
@@ -1,21 +1,17 @@
 'use strict';
 
-getEmitter.isStar = true;
-module.exports = getEmitter;
-
 class Listener {
-    constructor(event, context, handler, { several, through }) {
+    constructor(event, context, handler, { several = -1, through = 1 }) {
         this.event = event;
         this.context = context;
         this.handler = handler;
         this.several = several;
         this.through = through;
-        this.on = true;
         this.count = 0;
     }
 
     notify() {
-        if (this.on && (this.several - this.count !== 0)) {
+        if (this.several - this.count !== 0) {
             if (this.count % this.through === 0) {
                 this.handler.call(this.context);
             }
@@ -24,37 +20,25 @@ class Listener {
     }
 }
 
-function getEmitter() {
+module.exports = function () {
     return {
         listeners: [],
 
-        listen: function (event, context, handler, options) {
+        on: function (event, context, handler, options = {}) {
             this.listeners.push(new Listener(event, context, handler, options));
 
             return this;
         },
 
-        on: function (event, context, handler) {
-            return this.listen(event, context, handler, { several: -1, through: 1 });
-        },
-
         off: function (event, context) {
-            this.listeners.forEach(listener => {
-                if (listener.context === context && (listener.event.startsWith(event + '.') ||
-                    listener.event === event)) {
-                    listener.on = false;
-                }
-            });
+            this.listeners = this.listeners.filter(item => !(item.context === context &&
+                (item.event === event || item.event.startsWith(event + '.'))));
 
             return this;
         },
 
         emit: function (event) {
-            this.listeners.forEach(item => {
-                if (item.event === event) {
-                    item.notify();
-                }
-            });
+            this.listeners.filter(item => item.event === event).forEach(item => item.notify());
 
             if (event.includes('.')) {
                 this.emit(event.slice(0, event.lastIndexOf('.')));
@@ -64,19 +48,13 @@ function getEmitter() {
         },
 
         several: function (event, context, handler, times) {
-            if (times > 0) {
-                return this.listen(event, context, handler, { several: times, through: 1 });
-            }
-
-            return this.on(event, context, handler);
+            return this.on(event, context, handler, { several: times > 0 ? times : -1 });
         },
 
         through: function (event, context, handler, frequency) {
-            if (frequency > 0) {
-                return this.listen(event, context, handler, { several: -1, through: frequency });
-            }
-
-            return this.on(event, context, handler);
+            return this.on(event, context, handler, { through: frequency > 0 ? frequency : 1 });
         }
     };
-}
+};
+
+module.exports.isStar = true;
